eb819bc1983c485962338e8a8cbd0dbd81555834
Imel.17_ <68175852+melati17@users.noreply.github.com>
2023-12-27 22:12:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,18 +7,18 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'l3G9Qv5e6juaIsn',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "IMELOKAL bot"
+global.namaowner = "Melati"
+global.packname = IMELOKAL bot
+global.author = Melati
+global.myweb = "https://imelokal.shop"
+global.footer_text = "© " IMELOKAL bot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['083142405336']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
